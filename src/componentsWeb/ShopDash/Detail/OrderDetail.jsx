a88a77519a736626f9735c63660a988e55776a0e
Rikikudo <trunghieu99tt@gmail.com>
2020-07-02 15:42:39 +0700

    .

--- src/componentsWeb/ShopDash/Detail/OrderDetail.jsx
+++ src/componentsWeb/ShopDash/Detail/OrderDetail.jsx
@@ -7,179 +7,180 @@ import TextAreaDash from "../Form/TextAreaDash";
 import UploadFileDash from "../Form/UploadFileDash";
 
 const ItemDetail = ({ item, resetItem, handleEditItem, deleteItem }) => {
-	const [itemInfo, setOrderInfo] = useState(item);
-	const [isEdit, setIsEdit] = useState(false);
+    const [itemInfo, setOrderInfo] = useState(item);
+    const [isEdit, setIsEdit] = useState(false);
 
-	const options = ["Pending", "Processing", "Completed"];
+    const options = ["Pending", "Processing", "Completed"];
 
-	const toggleEdit = () => setIsEdit(!isEdit);
+    const toggleEdit = () => setIsEdit(!isEdit);
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		handleEditItem(itemInfo);
-	};
+    const onSubmit = (event) => {
+        event.preventDefault();
+        handleEditItem(itemInfo);
+    };
 
-	const onFieldChange = (event) => {
-		const newItemInfo = {
-			...itemInfo,
-			[event.target.name]: event.target.value,
-		};
-		setOrderInfo(newItemInfo);
-	};
+    const onFieldChange = (event) => {
+        const newItemInfo = {
+            ...itemInfo,
+            [event.target.name]: event.target.value,
+        };
+        setOrderInfo(newItemInfo);
+    };
 
-	const onChangeDate = (date, dateString) => {
-		const newItemInfo = {
-			...itemInfo,
-			purchasedOn: dateString,
-		};
-		setOrderInfo(newItemInfo);
-	};
+    const onChangeDate = (date, dateString) => {
+        const newItemInfo = {
+            ...itemInfo,
+            purchasedOn: dateString,
+        };
+        setOrderInfo(newItemInfo);
+    };
 
-	const onChangeOptions = (event) => {
-		const newItemInfo = {
-			...itemInfo,
-			status: event.target.value,
-		};
-		setOrderInfo(newItemInfo);
-	};
+    const onChangeOptions = (event) => {
+        const newItemInfo = {
+            ...itemInfo,
+            status: event.target.value,
+        };
 
-	// Config Data
+        setOrderInfo(newItemInfo);
+    };
 
-	const {
-		itemID,
-		name,
-		purchasedOn,
-		customer,
-		shipTo,
-		basePrice,
-		purchasedPrice,
-		status,
-	} = itemInfo;
+    // Config Data
 
-	console.log("Object.entries(itemInfo)", Object.entries(itemInfo));
+    const {
+        itemID,
+        name,
+        purchasedOn,
+        customer,
+        shipTo,
+        basePrice,
+        purchasedPrice,
+        status,
+    } = itemInfo;
 
-	const dataFields = itemInfo && Object.entries(itemInfo);
+    console.log("Object.entries(itemInfo)", Object.entries(itemInfo));
 
-	return (
-		<div className="itemDetail" onSubmit={onSubmit}>
-			{dataFields?.length && dataFields.map((item) => {})}
+    const dataFields = itemInfo && Object.entries(itemInfo);
 
-			<InputDash
-				type="text"
-				name="itemID"
-				value={itemID}
-				labelName="Order"
-				disabled
-			/>
-			<InputDash
-				type="text"
-				name="name"
-				placeHolder="Name"
-				labelName="Name"
-				value={name}
-				disabled={!isEdit}
-				onChange={onFieldChange}
-			/>
+    return (
+        <div className="itemDetail" onSubmit={onSubmit}>
+            {dataFields?.length && dataFields.map((item) => {})}
 
-			<div className="form-group">
-				<p>Purchased On</p>
-				<DatePicker
-					defaultValue={moment(purchasedOn, "DD/MM/YYYY")}
-					format="DD/MM/YYYY"
-					disabled={!isEdit}
-					onChange={onChangeDate}
-				/>
-			</div>
+            <InputDash
+                type="text"
+                name="itemID"
+                value={itemID}
+                labelName="Order"
+                disabled
+            />
+            <InputDash
+                type="text"
+                name="name"
+                placeHolder="Name"
+                labelName="Name"
+                value={name}
+                disabled={!isEdit}
+                onChange={onFieldChange}
+            />
 
-			<InputDash
-				type="text"
-				name="customer"
-				placeHolder="Customer"
-				labelName="Customer"
-				value={customer}
-				disabled={!isEdit}
-				onChange={onFieldChange}
-			/>
-			<InputDash
-				type="text"
-				name="shipTo"
-				placeHolder="Ship To"
-				labelName="Ship to"
-				value={shipTo}
-				disabled={!isEdit}
-				onChange={onFieldChange}
-			/>
+            <div className="form-group">
+                <p>Purchased On</p>
+                <DatePicker
+                    defaultValue={moment(purchasedOn, "DD/MM/YYYY")}
+                    format="DD/MM/YYYY"
+                    disabled={!isEdit}
+                    onChange={onChangeDate}
+                />
+            </div>
 
-			<InputDash
-				type="text"
-				name="basePrice"
-				placeHolder="Base Price"
-				labelName="Base Price"
-				value={basePrice}
-				disabled={!isEdit}
-				onChange={onFieldChange}
-			/>
-			<InputDash
-				type="text"
-				name="purchasedPrice"
-				placeHolder="Purchased Price"
-				labelName="Purchased Price"
-				value={purchasedPrice}
-				disabled={!isEdit}
-				onChange={onFieldChange}
-			/>
+            <InputDash
+                type="text"
+                name="customer"
+                placeHolder="Customer"
+                labelName="Customer"
+                value={customer}
+                disabled={!isEdit}
+                onChange={onFieldChange}
+            />
+            <InputDash
+                type="text"
+                name="shipTo"
+                placeHolder="Ship To"
+                labelName="Ship to"
+                value={shipTo}
+                disabled={!isEdit}
+                onChange={onFieldChange}
+            />
 
-			<UploadFileDash />
+            <InputDash
+                type="text"
+                name="basePrice"
+                placeHolder="Base Price"
+                labelName="Base Price"
+                value={basePrice}
+                disabled={!isEdit}
+                onChange={onFieldChange}
+            />
+            <InputDash
+                type="text"
+                name="purchasedPrice"
+                placeHolder="Purchased Price"
+                labelName="Purchased Price"
+                value={purchasedPrice}
+                disabled={!isEdit}
+                onChange={onFieldChange}
+            />
 
-			<OptionsDash
-				name="status"
-				options={options}
-				disabled={!isEdit}
-				value={status}
-				onChange={onChangeOptions}
-			/>
+            <UploadFileDash />
 
-			<TextAreaDash id="TextArea" name="Note" disabled={!isEdit} />
-			{(isEdit && (
-				<React.Fragment>
-					<button
-						type="submit"
-						className="btn btn-success mr-2"
-						onClick={onSubmit}
-					>
-						Submit
-					</button>
-					<button className="btn btn-light" onClick={toggleEdit}>
-						{" "}
-						Cancel
-					</button>
-				</React.Fragment>
-			)) || (
-				<React.Fragment>
-					<button
-						className="btn btn-success mr-2"
-						onClick={toggleEdit}
-						type="button"
-					>
-						Edit
-					</button>
-					<button
-						className="btn btn-light"
-						type="button"
-						onClick={() => {
-							deleteItem(itemInfo);
-							resetItem();
-						}}
-					>
-						Delete
-					</button>
-					<button className="btn btn-light" onClick={resetItem}>
-						Back
-					</button>
-				</React.Fragment>
-			)}
-		</div>
-	);
+            <OptionsDash
+                name="status"
+                options={options}
+                disabled={!isEdit}
+                value={status}
+                onChange={onChangeOptions}
+            />
+
+            <TextAreaDash id="TextArea" name="Note" disabled={!isEdit} />
+            {(isEdit && (
+                <React.Fragment>
+                    <button
+                        type="submit"
+                        className="btn btn-success mr-2"
+                        onClick={onSubmit}
+                    >
+                        Submit
+                    </button>
+                    <button className="btn btn-light" onClick={toggleEdit}>
+                        {" "}
+                        Cancel
+                    </button>
+                </React.Fragment>
+            )) || (
+                <React.Fragment>
+                    <button
+                        className="btn btn-success mr-2"
+                        onClick={toggleEdit}
+                        type="button"
+                    >
+                        Edit
+                    </button>
+                    <button
+                        className="btn btn-light"
+                        type="button"
+                        onClick={() => {
+                            deleteItem(itemInfo);
+                            resetItem();
+                        }}
+                    >
+                        Delete
+                    </button>
+                    <button className="btn btn-light" onClick={resetItem}>
+                        Back
+                    </button>
+                </React.Fragment>
+            )}
+        </div>
+    );
 };
 
 export default ItemDetail;
